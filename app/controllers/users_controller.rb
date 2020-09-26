fde2c2983eb85726706e8a75c3f5f08b0fbe4242
nogamieri <komeuma@gmail.com>
2020-09-26 11:08:07 +0000

    cssの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def favorites
     @user = User.find_by(id: params[:id])
     @favorites = Favorite.where(user_id: @user.id)
+    @buys = Buy.all.order("created_at DESC")
   end
 
 
